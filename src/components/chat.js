e9f95638317e617c2fe752a02cf3512c438b9d70
emmanuel-olateju <olatejuemmanuel@gmail.com>
2023-07-22 08:35:19 +0100

    interface
    chat interface complete

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,70 +1,122 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
+import React, { useRef, useEffect, useState } from 'react';
+// import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Chat = () => {
+  // let messages = [];
+  // let inputs = [];
   const [messages, setMessages] = useState([]);
+  const [inputs, setInputs] = useState([]);
   const [inputText, setInputText] = useState('');
+  const [receivedData, setReceivedData] = useState('');
+  const [btnState, setBtnState] = useState();
+  // let receivedData = "waiting for your input";
+  const [error, setError] = useState(null);
+  const divRef = useRef(null);
+
+  const gptResponseURL = 'http://127.0.0.1:5000/gptRespond';
+  const gptQueryURL = 'http://127.0.0.1:5000/sendQuery';
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
+  useEffect(() => {
     if (inputText.trim() !== '') {
-      sendMessage(inputText);
-      setInputText('');
-    }
-  };
+      //Add new user input to inputs
+      let newInput = {
+        id:inputs.length+1,
+        text:inputText,
+        sender:'user',
+      }
+      setInputs([...inputs, inputText]);
 
-  const sendMessage = (text) => {
-    const newMessage = {
-      id: messages.length + 1,
-      text,
-      sender: 'user',
-    };
+      //Send input to server
 
-    setMessages([...messages, newMessage]);
+      //Fetch to input from server
+      fetch(gptResponseURL,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'query':inputText}),
+      })
+        .then(res => res.text())
+        .then(
+          data => {
+            setReceivedData(data)
+            // console.log(receivedData);
+            setError(null);
+          })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          setError('Error fetching data. Please try again.');
+        });
 
-    // Simulate ChatGPT response
-    setTimeout(() => {
-      const response = generateChatGPTResponse(text);
-      const botMessage = {
-        id: messages.length + 2,
+      const response = generateChatGPTResponse();
+      const newMessage = {
+        id: newInput.id,
         text: response,
         sender: 'bot',
       };
-      setMessages([...messages, botMessage]);
-    }, 500);
+      setMessages([...messages, newMessage]);
+      console.log([newMessage.text]);
+
+      setInputText('');
+
+      const chatDiv = document.querySelector('.chat-messages');
+      if(chatDiv){
+        divRef.current = chatDiv;
+        divRef.current.insertAdjacentHTML(
+          'beforeend',
+          `<div><div class="user">${newInput.text}</div><div class="bot">${newMessage.text}</div></div>`
+        )
+      }
+
+      // messages.map((member) => {
+      //   console.log(member.id);
+      // });
+    }
+  }, [btnState]);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  const handleClick = (event) => {
+    const { name, value } = event.target
+    setBtnState((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const generateChatGPTResponse = (userMessage) => {
     // Implement your response generation logic here
-    // You can use external AI models or predefined responses based on user input
-    // For simplicity, we'll return a static response
-    return "I'm sorry, I'm just a mimic. I don't have actual ChatGPT capabilities.";
+    return receivedData;
+    // return receivedData;
   };
 
   return (
     <div className="chat-container">
       <div className="chat-messages">
-        {messages.map((message) => (
-          <div key={message.id} className={`message ${message.sender}`}>
-            {message.text}
-          </div>
-        ))}
+
       </div>
       <form className="chat-form" onSubmit={handleFormSubmit}>
+        <></>
         <div className="chat-form-input">
             <textarea
             type="text"
-            placeholder="Type a message..."
+            placeholder=""
             value={inputText}
             onChange={handleInputChange}
             >
             </textarea>
-            <Button type="submit" variant="contained" color="primary">Send</Button>
+            <a href="#"><button type="submit" onClick={handleClick}>
+              <ArrowForwardIcon />
+            </button></a>
         </div>
+        <></>
       </form>
     </div>
   );
